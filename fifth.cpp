3fb7090a38105c30e296788b437756bec5cec00f
gauritechcode <gaurinimbalkar2003@gmail.com>
2025-05-19 08:27:24 +0530

    cpp if else example

---
+++ fifth.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout<<"enter the value a ";
+cin >>a;
+cout<<"enter the value b ";
+cin >>b;
+if(a>b){
+  cout<<"a is grater "<<a;
+}else{
+cout<<"b is greater "<<b;
+
+}
+return 0;
+
+
+
+
+
+
+
+
+}
