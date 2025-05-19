293c3a1304c4ce7bf970f345adf4658bf2feeff1
gauritechcode <gaurinimbalkar2003@gmail.com>
2025-05-19 08:25:34 +0530

    cin example

---
+++ Second.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int age;
+string name;
+cout<<"enter your age";
+cin >>age;
+cout<< "your age "<<age<<endl;
+
+cout<<"enter your name";
+cin >>name;
+cout<< "your name"<<name;
+
+ return 0;
+}
