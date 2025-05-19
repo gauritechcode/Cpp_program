1f8a66daa99ad29cc8fb834f68808c142dc8170c
gauritechcode <gaurinimbalkar2003@gmail.com>
2025-05-19 08:26:53 +0530

    cpp float example

---
+++ fourth.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float x,y,z;
+cout<<"enter the value x ";
+cin >>x;
+cout<<"enter the value y ";
+cin >>y;
+cout<<"enter the value z ";
+cin >>z;
+float u=x+y*z;
+cout<<"addition of x+y*z= "<<u<<endl;
+float v=x-y-z;
+cout<<"subtraction of x-y-z= "<<v;
+return 0;
+
+}
