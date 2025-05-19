22ddc8168e64c1611a137782d92ac026faa22fce
gauritechcode <gaurinimbalkar2003@gmail.com>
2025-05-19 08:26:21 +0530

    cpp int example

---
+++ third.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout<<"enter the value a ";
+cin >>a;
+cout<<"enter the value b ";
+cin >>b;
+int c=a+b;
+cout<<"addition of a+b= "<<c<<endl;
+int d=a-b;
+cout<<"subtraction of a-b= "<<d<<endl;
+int e=a*b;
+cout<<"multiplication of a*b= "<<e<<endl;
+int f=a/b;
+cout<<"division of a/b= "<<f;
+return 0;
+
+}
