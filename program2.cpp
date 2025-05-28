78d8c35544a25ebe0dbcd58a9394d4bc257945ec
gauritechcode <gaurinimbalkar2003@gmail.com>
2025-05-28 08:15:17 +0530

    Multiple char program

---
+++ program2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+char a[10];
+char b[20];
+char c[30];
+
+cout<<"enter the character";
+cin >>a;
+cout<<"enter the character";
+cin >>b;
+cout<<"enter the character";
+cin >>c;
+cout<<"FirstName ="<<a<<endl<<"MiddleName ="<<b<<endl<<"LastName ="<<c;
+return 0;
+
+
+}
