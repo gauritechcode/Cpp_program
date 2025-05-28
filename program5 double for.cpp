c99ae3f3e7f229dff0c096f8441ac608e7ea2d02
gauritechcode <gaurinimbalkar2003@gmail.com>
2025-05-28 08:19:06 +0530

    double for program

---
+++ program5 double for.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a;
+cout<<"enter the value";
+cin >>a;
+for(int i=0;i<=a;i++)
+{
+for(int j=0;j<=a;j++)
+{
+
+cout<<" "<<i;
+
+}
+cout<<endl;
+
+}
+
+
+
+
+
+
+
+
+
+
+}
