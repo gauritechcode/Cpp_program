b8801bf738f35b79304149ac7048f41be4e3900d
gauritechcode <gaurinimbalkar2003@gmail.com>
2025-05-29 08:17:34 +0530

    switch program.cpp

--- program1.cpp
+++ program1.cpp
@@ -2,9 +2,56 @@
 using namespace std;
 int main()
 {
-char a;
-cout<<"enter the  character";
+int a;
+cout<<"Enter the number 1 to 10 ";
 cin >>a;
-cout<<"enter character "<<a;
+switch(a)
+{
+case 1:
+    cout<<"One";
+    break;
+
+case 2:
+    cout<<"Two";
+    break;
+
+case 3:
+    cout<<"Three";
+    break;
+
+
+case 4:
+    cout<<"Four";
+    break;
+
+case 5:
+    cout<<"Five";
+    break;
+
+case 6:
+    cout<<"Six";
+    break;
+
+case 7:
+    cout<<"Seven";
+    break;
+
+case 8:
+    cout<<"Eight";
+    break;
+
+case 9:
+    cout<<"Nine";
+    break;
+
+case 10:
+    cout<<"Ten";
+    break;
+
+default :
+    cout<<"Please try again";
+
+
+}
 
 }
